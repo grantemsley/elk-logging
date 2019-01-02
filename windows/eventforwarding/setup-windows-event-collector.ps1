b4d068601f195b2e5c6cfd3b78b3fe38155ca11c
Grant Emsley <grant@emsley.ca>
2019-01-02 18:22:29 -0500

    Fix domain controllers being included in forwarding, we use winlogbeat directly on them

--- windows/eventforwarding/setup-windows-event-collector.ps1
+++ windows/eventforwarding/setup-windows-event-collector.ps1
@@ -11,6 +11,10 @@ wevtutil sl forwardedevents /ms:512000000
 # Run quick config for subscription service
 wecutil qc -quiet
 
+# Make WinRM and WecSvc get their own process instead of being grouped with other services
+sc.exe config WinRM type=own
+sc.exe config WecSvc type=own
+
 # Import the subscriptions
 Get-ChildItem .\subscriptions -Filter *.xml | Foreach-Object {
     Write-Host Creating subscription $_.FullName
